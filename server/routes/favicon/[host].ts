bce19f6ce8d2ffb2fb306d8bf8df602dafd66691
一纸忘忧 <i@ikxin.com>
2025-02-07 23:11:20 +0800

    feat(favicon): create multiple new favicon images for fallback

--- server/routes/favicon/[host].ts
+++ server/routes/favicon/[host].ts
@@ -4,11 +4,7 @@ import icoToPng from "ico-to-png";
 export default defineResponseHandler(async (event) => {
   const host = getRouterParam(event, "host");
 
-  const defaultIco = await generateStringIco(host?.charAt(0).toUpperCase());
-
-  if (!host) {
-    return defaultIco;
-  }
+  const icoString = host!.charAt(0).toLocaleLowerCase();
 
   try {
     return await getIcoByLinkTag(host);
@@ -18,61 +14,45 @@ export default defineResponseHandler(async (event) => {
     return await getIcoByFavicon(host);
   } catch (error) {}
 
-  return defaultIco;
+  const data = await useStorage("assets:server").getItemRaw(
+    `favicon/${/^[0-9a-z]$/.test(icoString) ? icoString : "="}.png`
+  );
+
+  return data;
 });
 
-const LINK_TAG_REGEX =
+const LINK_REGEX =
   /((<link[^>]+rel=.(icon|shortcut icon|alternate icon|apple-touch-icon)[^>]+>))/i;
 
-const LINK_REF_REGEX = /href=["']([^"']+)["']/i;
+const HREF_REGEX = /href=["']([^"']+)["']/i;
 
-const RESIZE_OPTIONS = { width: 64, height: 64 };
+const getIcoByLinkTag = async (host?: string) => {
+  const html = await fetch(`http://${host}`).then((res) => res.text());
 
-const getIcoByLinkTag = async (host: string): Promise<Buffer> => {
-  const htmlStr = await fetch(`http://${host}`).then((res) => res.text());
+  const link = html.match(LINK_REGEX);
+  if (!link) throw new Error();
 
-  const linkTag = htmlStr.match(LINK_TAG_REGEX);
-  if (!linkTag) throw new Error();
+  const href = link[1].match(HREF_REGEX);
+  if (!href) throw new Error();
 
-  const linkRef = linkTag[1].match(LINK_REF_REGEX);
-  if (!linkRef) throw new Error();
-
-  let [_, iconUrl] = linkRef;
+  let [_, iconUrl] = href;
   iconUrl = new URL(iconUrl, `http://${host}`).toString();
 
-  const data = await fetch(iconUrl).then((res) => res.arrayBuffer());
-  const buffer = Buffer.from(data);
+  const buffer = Buffer.from(
+    await fetch(iconUrl).then((res) => res.arrayBuffer())
+  );
 
   if (iconUrl.endsWith(".ico")) {
-    return icoToPng(Buffer.from(buffer), 64);
+    return icoToPng(buffer, 64);
+  } else {
+    return sharp(buffer).resize({ width: 64, height: 64 }).toBuffer();
   }
-
-  return sharp(Buffer.from(buffer)).resize(RESIZE_OPTIONS).png().toBuffer();
 };
 
-const getIcoByFavicon = async (host: string): Promise<Buffer> => {
-  const data = await fetch(`http://${host}/favicon.ico`).then((res) =>
-    res.arrayBuffer()
-  );
-  return await icoToPng(Buffer.from(data), 32);
-};
-
-const generateStringIco = (val?: string): Promise<Buffer> => {
-  const [r, g, b] = Array.from({ length: 3 }, () =>
-    Math.floor(Math.random() * 200)
+const getIcoByFavicon = async (host?: string) => {
+  const buffer = Buffer.from(
+    await fetch(`http://${host}/favicon.ico`).then((res) => res.arrayBuffer())
   );
 
-  const svg = `<svg width="64" height="64">
-    <rect width="64" height="64" rx="16" ry="16" fill="rgb(${r},${g},${b})"/>
-    <text 
-      x="32"
-      y="44"
-      font-size="36"
-      font-weight="bold"
-      fill="#FFFFFF"
-      text-anchor="middle"
-    >${process.env.VERCEL ? "" : val}</text>
-  </svg>`;
-
-  return sharp(Buffer.from(svg)).png().toBuffer();
+  return icoToPng(buffer, 64);
 };
