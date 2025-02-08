fe752bf0cf433c9a7a2a11f36dbb32209d664064
一纸忘忧 <i@ikxin.com>
2025-02-08 10:23:47 +0800

    feat(telegram): add telegram avatar fetch route

---
+++ server/routes/telegram/[user].ts
@@ -0,0 +1,17 @@
+export default defineResponseHandler(async (event) => {
+  const user = getRouterParam(event, "user");
+
+  try {
+    const fetchUrl = `https://t.me/i/userpic/320/${user}.jpg`;
+    const response = await fetch(fetchUrl);
+
+    if (response.ok) {
+      return Buffer.from(await response.arrayBuffer());
+    } else {
+      throw new Error();
+    }
+  } catch {
+    const fallbackKey = `telegram/fallback.png`;
+    return useStorage("assets:server").getItemRaw(fallbackKey);
+  }
+});
