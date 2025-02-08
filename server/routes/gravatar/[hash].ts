a2cc17c7df9c30d0cf26928393ef47fd6bd28310
一纸忘忧 <i@ikxin.com>
2025-02-08 09:28:25 +0800

    feat(gravatar): add gravatar avatar fetch route

---
+++ server/routes/gravatar/[hash].ts
@@ -0,0 +1,20 @@
+export default defineResponseHandler(async (event) => {
+  const hash = getRouterParam(event, "hash");
+  const { size = 100, s = 100 } = getQuery(event);
+
+  try {
+    const fetchUrl = `https://secure.gravatar.com/avatar/${hash}.png?s=${
+      size || s || 100
+    }`;
+    const response = await fetch(fetchUrl);
+
+    if (response.ok) {
+      return Buffer.from(await response.arrayBuffer());
+    } else {
+      throw new Error();
+    }
+  } catch {
+    const fallbackKey = `gravatar/fallback.png`;
+    return useStorage("assets:server").getItemRaw(fallbackKey);
+  }
+});
