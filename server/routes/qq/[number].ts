969ee6f9c6ec4bd17bfa8f4b12b773141fda3cc5
一纸忘忧 <i@ikxin.com>
2025-02-08 09:46:45 +0800

    feat(qq): add qq avatar fetch route

---
+++ server/routes/qq/[number].ts
@@ -0,0 +1,20 @@
+export default defineResponseHandler(async (event) => {
+  const number = getRouterParam(event, "number");
+  const { s, spec } = getQuery(event);
+
+  try {
+    const fetchUrl = `https://q1.qlogo.cn/g?b=qq&nk=${number}&s=${
+      s || spec || 100
+    }`;
+    const response = await fetch(fetchUrl);
+
+    if (response.ok) {
+      return Buffer.from(await response.arrayBuffer());
+    } else {
+      throw new Error();
+    }
+  } catch {
+    const fallbackKey = `qq/fallback.png`;
+    return useStorage("assets:server").getItemRaw(fallbackKey);
+  }
+});
